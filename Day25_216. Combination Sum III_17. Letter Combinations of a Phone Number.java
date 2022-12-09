e4deeb4ca223cfe24f6078f217bead50de8616f3
huiwei95 <90156480+huiwei95@users.noreply.github.com>
2022-12-09 18:43:57 -0500

    Update Day25_216. Combination Sum III_17. Letter Combinations of a Phone Number.java

--- Day25_216. Combination Sum III_17. Letter Combinations of a Phone Number.java
+++ Day25_216. Combination Sum III_17. Letter Combinations of a Phone Number.java
@@ -73,43 +73,4 @@ class Solution {
         backtracking(digits, numString, 0);
         return result;
     }
-}class Solution {
-    List<String> result = new ArrayList<>();
-    StringBuilder s = new StringBuilder();
-
-    public void backtracking(String digits, String[] numString, int numIndex) {
-        if (numIndex == digits.length()) {
-            result.add(s.toString());
-            return;
-        }
-        // s 表示当前num对应的字符串
-        String str = numString[digits.charAt(numIndex) - '0'];
-        for (int i = 0; i < str.length(); i ++) {
-            s.append(str.charAt(i));
-            backtracking(digits, numString, numIndex + 1);
-            s.deleteCharAt(s.length() - 1);
-        }
-
-    }
-
-    public List<String> letterCombinations(String digits) {
-        String[] numString = {
-            " ", // 0
-            " ", // 1
-            "abc", // 2
-            "def", // 3
-            "ghi", // 4
-            "jkl", // 5
-            "mno", // 6
-            "pqrs", // 7
-            "tuv", // 8
-            "wxyz", // 9
-        } ;
-
-        if (digits == null || digits.length() == 0) {
-            return result;
-        }
-        backtracking(digits, numString, 0);
-        return result;
-    }
 }
