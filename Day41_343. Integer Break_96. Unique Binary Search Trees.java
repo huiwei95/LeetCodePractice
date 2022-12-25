ea118cedc891a7495637e856818cb98d846ea08a
huiwei95 <90156480+huiwei95@users.noreply.github.com>
2022-12-25 14:11:09 -0500

    Add files via upload

---
+++ Day41_343. Integer Break_96. Unique Binary Search Trees.java
@@ -0,0 +1,41 @@
+// 343. Integer Break
+class Solution {
+    public int integerBreak(int n) {
+        //dp[i] 为正整数 i 拆分后的结果的最大乘积
+        int[] dp = new int[n+1];
+        dp[2] = 1;
+        for(int i = 3; i <= n; i++) {
+            for(int j = 1; j <= i-j; j++) {
+                // 这里的 j 其实最大值为 i-j,再大只不过是重复而已，
+                //并且，在本题中，我们分析 dp[0], dp[1]都是无意义的，
+                //j 最大到 i-j,就不会用到 dp[0]与dp[1]
+                dp[i] = Math.max(dp[i], Math.max(j*(i-j), j*dp[i-j]));
+                // j * (i - j) 是单纯的把整数 i 拆分为两个数 也就是 i,i-j ，再相乘
+                //而j * dp[i - j]是将 i 拆分成两个以及两个以上的个数,再相乘。
+            }
+        }
+        return dp[n];
+    }
+}
+
+
+
+
+
+// 96. Unique Binary Search Trees
+class Solution {
+    public int numTrees(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i ++) {
+            for (int j = 1; j <= i; j ++) {
+                // 对于第i个节点，需要考虑1作为根节点直到i作为根节点的情况，所以需要累加
+                // 一共i个节点，对于根节点j时,左子树的节点个数为j-1，右子树的节点个数为i-j
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+        return dp[n];
+    }
+}
