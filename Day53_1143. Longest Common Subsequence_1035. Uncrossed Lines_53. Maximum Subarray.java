d991feda1b3c684a40131b2c2b6902422798699f
huiwei95 <90156480+huiwei95@users.noreply.github.com>
2023-01-06 11:44:20 -0500

    Add files via upload

---
+++ Day53_1143. Longest Common Subsequence_1035. Uncrossed Lines_53. Maximum Subarray.java
@@ -0,0 +1,62 @@
+// 1143. Longest Common Subsequence
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int[][] dp = new int[text1.length() + 1][text2.length() + 1];
+        for (int i = 1; i <= text1.length(); i ++) {
+            char char1 = text1.charAt(i - 1);
+            for (int j = 1; j <= text2.length(); j ++) {
+                char char2 = text2.charAt(j - 1);
+                if (char1 == char2) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[text1.length()][text2.length()];
+    }
+}
+
+
+
+
+
+// 1035. Uncrossed Lines
+class Solution {
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        int[][] dp = new int[nums1.length + 1][nums2.length + 1];
+        for (int i = 1; i <= nums1.length; i++) {
+            for (int j = 1; j <= nums2.length; j++) {
+                if (nums1[i - 1] == nums2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[nums1.length][nums2.length];
+    }
+}
+
+
+
+
+
+// 53. Maximum Subarray
+class Solution {
+    public int maxSubArray(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int res = nums[0];
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+            if(dp[i] > res) res = dp[i]; // res 保存dp[i]的最大值
+        }
+        return res;
+    }
+}
