b63c1b234819f6c5b8d2859d5e24b1602c1bf0cd
huiwei95 <90156480+huiwei95@users.noreply.github.com>
2023-01-08 09:43:15 -0500

    Add files via upload

---
+++ Day55_392. Is Subsequence_115. Distinct Subsequences.java
@@ -0,0 +1,47 @@
+// 392. Is Subsequence
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int length1 = s.length(); int length2 = t.length();
+        int[][] dp = new int[length1+1][length2+1];
+        for(int i = 1; i <= length1; i++){
+            for(int j = 1; j <= length2; j++){
+                if(s.charAt(i-1) == t.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }else{
+                    dp[i][j] = dp[i][j-1];
+                }
+            }
+        }
+        if(dp[length1][length2] == length1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
+
+
+
+
+
+// 115. Distinct Subsequences
+class Solution {
+    public int numDistinct(String s, String t) {
+        int[][] dp = new int[s.length() + 1][t.length() + 1];
+        for (int i = 0; i < s.length() + 1; i++) {
+            dp[i][0] = 1;
+        }
+        
+        for (int i = 1; i < s.length() + 1; i++) {
+            for (int j = 1; j < t.length() + 1; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                }else{
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        
+        return dp[s.length()][t.length()];
+    }
+}
