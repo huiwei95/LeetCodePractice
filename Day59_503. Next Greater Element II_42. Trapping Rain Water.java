e481e3ced5232f5e6540be99811591a6d09e366f
huiwei95 <90156480+huiwei95@users.noreply.github.com>
2023-01-12 12:17:51 -0500

    Add files via upload

---
+++ Day59_503. Next Greater Element II_42. Trapping Rain Water.java
@@ -0,0 +1,51 @@
+// 503. Next Greater Element II
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        //边界判断
+        if(nums == null || nums.length <= 1) {
+            return new int[]{-1};
+        }
+        int size = nums.length;
+        int[] result = new int[size];//存放结果
+        Arrays.fill(result,-1);//默认全部初始化为-1
+        Stack<Integer> st= new Stack<>();//栈中存放的是nums中的元素下标
+        for(int i = 0; i < 2*size; i++) {
+            while(!st.empty() && nums[i % size] > nums[st.peek()]) {
+                result[st.peek()] = nums[i % size];//更新result
+                st.pop();//弹出栈顶
+            }
+            st.push(i % size);
+        }
+        return result;
+    }
+}
+
+
+
+
+
+// 42. Trapping Rain Water
+class Solution {
+    public int trap(int[] height) {
+        int length = height.length;
+        if (length <= 2) return 0;
+        int[] maxLeft = new int[length];
+        int[] maxRight = new int[length];
+        
+        // 记录每个柱子左边柱子最大高度
+        maxLeft[0] = height[0];
+        for (int i = 1; i< length; i++) maxLeft[i] = Math.max(height[i], maxLeft[i-1]);
+        
+        // 记录每个柱子右边柱子最大高度
+        maxRight[length - 1] = height[length - 1];
+        for(int i = length - 2; i >= 0; i--) maxRight[i] = Math.max(height[i], maxRight[i+1]);
+        
+        // 求和
+        int sum = 0;
+        for (int i = 0; i < length; i++) {
+            int count = Math.min(maxLeft[i], maxRight[i]) - height[i];
+            if (count > 0) sum += count;
+        }
+        return sum;
+    }
+}
